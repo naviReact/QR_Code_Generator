5cd9031f3104fc3cc592597a6eed95ef6e0c01f0
Sanju Mahto <sanjumahto328@gmail.com>
2023-11-26 13:53:22 +0530

    ui update

--- src/modules/Qr.js
+++ src/modules/Qr.js
@@ -1,17 +1,17 @@
-import { React, useEffect, useState } from 'react';
-import './Qr.css';
-import Image from '../Images/images.png';
+import { React, useEffect, useState } from "react";
+import "./Qr.css";
+import Image from "../Images/images.png";
 import ModSms from "../comp/Sms";
 import ModText from "../comp/Text";
 import ModUrl from "../comp/Url";
 import ModVcard from "../comp/Vcard";
-import ModEmail from '../comp/Email';
-import ModAppStore from '../comp/AppStore';
-import ModYoutube from '../comp/Youtube';
-import ModInsta from '../comp/Insta';
-import ModFacebook from '../comp/Facebook';
-import ModWifi from '../comp/Wifi';
-import ModPayment from '../comp/Payment';
+import ModEmail from "../comp/Email";
+import ModAppStore from "../comp/AppStore";
+import ModYoutube from "../comp/Youtube";
+import ModInsta from "../comp/Insta";
+import ModFacebook from "../comp/Facebook";
+import ModWifi from "../comp/Wifi";
+import ModPayment from "../comp/Payment";
 // import ModPdf from '../comp/Pdf';
 
 const menuItems = [
@@ -27,10 +27,7 @@ const menuItems = [
   { name: "AppStore", icon: "#", component: <ModAppStore /> },
   { name: "Payment", icon: "#", component: <ModPayment /> },
   // { name: "Pdf", icon: "#", component: <ModPdf/> },
-
 ];
-
-
 
 var download_image_name = "hehehe";
 
@@ -38,23 +35,21 @@ export const SetDownloadImageName = (n) => {
   download_image_name = n;
 };
 
-
 const Qr = () => {
-
   const [currentComponentIndex, setCurrentComponentIndex] = useState(0);
 
   useEffect(() => {
     console.log("currentComponentIndex : ", currentComponentIndex);
   }, [currentComponentIndex]);
 
-// return<div>test code of sanjufasdklfjhaksdjhf kajsd<p/>hf kjahsdjkf hasdkjfh kasjdhf kajsdhfk jhfsad<p></p>/fj haksjdhf kajsd<p></p>/fj haksjdhf kd<p></p>/fj haksjdhf kd<p></p>/fj haksjdhf kd<p></p>/fj haksjdhf kefhk</div>
+  // return<div>test code of sanjufasdklfjhaksdjhf kajsd<p/>hf kjahsdjkf hasdkjfh kasjdhf kajsdhfk jhfsad<p></p>/fj haksjdhf kajsd<p></p>/fj haksjdhf kd<p></p>/fj haksjdhf kd<p></p>/fj haksjdhf kd<p></p>/fj haksjdhf kefhk</div>
 
   return (
-    <div className='qr_parent'>
-      <div className='container-qr'>
-        <div className='container-wrapper'>
-          <div className='qr-links-parent'>
-          {/* <i className='fa fa-link'></i> */}
+    <div className="qr_parent">
+      <div className="container-qr">
+        <div className="container-wrapper">
+          <div className="qr-links-parent">
+            {/* <i className='fa fa-link'></i> */}
             {menuItems.map((val, i, self) => {
               const OnClick = () => {
                 setCurrentComponentIndex(i);
@@ -65,10 +60,11 @@ const Qr = () => {
                 </div>
               );
             })}
-
           </div>
         </div>
-        <div className='output-div'>{menuItems[currentComponentIndex].component}</div>
+        <div className="output-div">
+          {menuItems[currentComponentIndex].component}
+        </div>
       </div>
       <div className='preview'>
         <div className='preview-continer'>
@@ -79,21 +75,18 @@ const Qr = () => {
           </div>
         </div>
       </div>
-
     </div>
 
     // background-color: rgb(79, 70, 229);
     // background-color: rgb(67 56 202);
-
-
   );
 };
 
 const TEst_qu_Function = () => {
-  const a = document.createElement('a');
-  const qr_output = document.querySelector('.qu_div_image_generated');
+  const a = document.createElement("a");
+  const qr_output = document.querySelector(".qu_div_image_generated");
   a.href = qr_output.src;
-  a.download = download_image_name + '_qr_code.png';
+  a.download = download_image_name + "_qr_code.png";
   document.body.appendChild(a);
   a.click();
   document.body.removeChild(a);
